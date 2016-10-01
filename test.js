fc6a6efd466608b1b6c6029cd3732311554ccd2f
Robin Millette <robin@millette.info>
2016-10-01 09:32:54 -0400

    Run tests in series.

--- test.js
+++ test.js
@@ -6,12 +6,12 @@
 import test from 'ava'
 import fn from './'
 
-test('search', async t => {
+test.serial('search', async t => {
   const result = await fn.search('rollodeqc')
   t.is(result[0].repository_url.indexOf('https://github.com/millette/'), 0)
 })
 
-test('search paged', async t => {
+test.serial('search paged', async t => {
   const result = await fn.search({ q: 'rollodeqc', per_page: 5, raw: true })
   // console.log(result.headers)
   t.is(result.body.length, 5)
@@ -28,12 +28,12 @@ test('search paged', async t => {
   t.is(result3.body[0].repository_url.indexOf('https://github.com/millette/'), 0)
 })
 
-test('search raw', async t => {
+test.serial('search raw', async t => {
   const result = await fn.search({ q: 'rollodeqc', raw: true })
   t.is(result.body[0].repository_url.indexOf('https://github.com/millette/'), 0)
 })
 
-test('npm got', async t => {
+test.serial('npm got', async t => {
   const result = await fn.platform('npm', 'got')
   t.is(result.name, 'got')
   t.is(result.platform, 'NPM')
@@ -41,7 +41,7 @@ test('npm got', async t => {
   t.is(result.package_manager_url, 'https://www.npmjs.com/package/got')
 })
 
-test('npm got raw', async t => {
+test.serial('npm got raw', async t => {
   const result = await fn.platform('npm', 'got', { raw: true })
   const r2 = result.body
   // console.log(result.headers)
@@ -52,41 +52,41 @@ test('npm got raw', async t => {
   t.is(r2.package_manager_url, 'https://www.npmjs.com/package/got')
 })
 
-test('npm got dependencies', async t => {
+test.serial('npm got dependencies', async t => {
   const result = await fn.platform.dependencies('npm', 'got', '6.3.0')
   t.is(result.name, 'got') // FIXME
 })
 
-test('npm got dependencies', async t => {
+test.serial('npm got dependencies', async t => {
   const result = await fn.platform.dependencies('npm', 'got', '6.3.0', { raw: true })
   // console.log(result.headers)
   t.is(result.body.name, 'got') // FIXME
 })
 
-test('npm got dependents', async t => {
+test.serial('npm got dependents', async t => {
   const result = await fn.platform.dependents('npm', 'got')
   t.true(result.length > 5)
 })
 
-test('npm got dependent_repositories', async t => {
+test.serial('npm got dependent_repositories', async t => {
   const result = await fn.platform.dependent_repositories('npm', 'got')
   t.true(result.length > 5)
 })
 
-test('github', async t => {
+test.serial('github', async t => {
   const a = ['millette', 'rollodeqc-gh-utils']
   const result = await fn.github(a[0], a[1])
   t.is(result.full_name, a.join('/'))
 })
 
-test('github dependencies', async t => {
+test.serial('github dependencies', async t => {
   const a = ['millette', 'rollodeqc-gh-utils']
   const result = await fn.github.dependencies(a[0], a[1])
   t.is(result.full_name, a.join('/'))
   t.true(result.dependencies.length > 5)
 })
 
-test('github projects', async t => {
+test.serial('github projects', async t => {
   const a = ['millette', 'rollodeqc-gh-utils']
   const result = await fn.github.projects(a[0], a[1])
   t.true(result.length > 0)
@@ -94,7 +94,7 @@ test('github projects', async t => {
   t.is(result[0].platform, 'NPM')
 })
 
-test('github projects 404', async t => {
+test.serial('github projects 404', async t => {
   const a = ['kudlaj', 'jamendo-contest']
   const result = await fn.github.projects(a[0], a[1])
   t.is(result.length, 0)
